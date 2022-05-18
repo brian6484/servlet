7e32b5b8e52c1b60c3b5148e46429539c9cc94b7
Brian <brianlee6484@gmail.com>
2022-05-18 16:18:30 +0900

    Initial commit

---
+++ src/main/java/hello/demo/basic/request/RequestParamServlet.java
@@ -0,0 +1,25 @@
+package hello.demo.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name="requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName+"=" + req.getParameter(paramName)));
+
+//        if there are multiple usernames
+        String[] usernames = req.getParameterValues("username");
+        for (String username : usernames) {
+            System.out.println(username);
+        }
+        resp.getWriter().write("hi");
+    }
+}
